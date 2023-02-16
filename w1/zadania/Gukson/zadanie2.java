c31b562de6eaa184b6e4107bb6807ecede1c656c
Gukson <75785907+Gukson@users.noreply.github.com>
2023-02-16 18:36:25 +0100

    w1 - made

---
+++ w1/zadania/Gukson/zadanie2.java
@@ -0,0 +1,21 @@
+public class Zadanie2 {
+    public static void main(String[] args) {
+        int a,b;
+        a = 5;
+        b = 10;
+
+        double c,d;
+        c = Math.pow(a, b);
+        d = Math.sqrt(c);
+
+        System.out.println("a = " + a + " b = " + b + " c = " + c + " d = " + d);
+
+        int e = -5;
+        double g,h;
+        g = Math.abs(e);
+        h = 10.5001;
+        h = Math.round(h);
+
+        System.out.println("e = " + e + " g = " + g + " h = " + h);
+    }
+}
