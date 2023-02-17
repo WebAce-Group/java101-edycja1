b82f896a59f292b0076719b1cffff72bba82c6fb
Adrian Goral <adrian.goral@webace-group.dev>
2023-02-17 18:31:32 +0100

    Create .gitignore

---
+++ w1/zadania/RadekBil/Zadanie2.java
@@ -0,0 +1,18 @@
+import static java.lang.Math.*;
+public class Zadanie2 {
+    public static void main(String[] args){
+        int a=5, b=10, e=-5;
+        double c,d,g,h=10.5001;
+        c=pow(a,b); /*a do potegi b*/
+        d=sqrt(c); /*pierwiastek kwadratowy z c*/
+        e=abs(e);
+        h=+round(h);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(h);
+        System.out.println(e);
+
+    }
+}
