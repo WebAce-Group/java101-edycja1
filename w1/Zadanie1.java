1da948b6a368a04b3fd3f59545890e36134b05f4
Milosz Pietryszyn <milosz031018@gmail.com>
2023-02-18 23:05:23 +0100

    Rozwiązania zadań z zajęć 1

---
+++ w1/Zadanie1.java
@@ -0,0 +1,16 @@
+package w1;
+
+public class Zadanie1 {
+    public static void main(String[] args) {
+        int a = 1, b = 2, c = 3, d = 4;
+        int ab = a + b, cd = c - d;
+        System.out.println(ab);
+        double e=1.1, f=2.2, g=3.3, h=4.4;
+        double ef=e*f;
+        double gh=g/h;
+        int reszta=ab%b;
+        char i='A', j='a';
+        System.out.println(i);
+
+    }
+}
