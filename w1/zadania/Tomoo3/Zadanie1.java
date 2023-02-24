42bdb5ad9ad919aef2a7b6311bf8a488b98632e9
Tomasz Kurowicki <tomekkurowicki1@gmail.com>
2023-02-25 00:39:13 +0100

    Zadania

---
+++ w1/zadania/Tomoo3/Zadanie1.java
@@ -0,0 +1,45 @@
+package w1.zadania.Tomoo3;
+
+public class Zadanie1 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int ab = a + b;
+        int cd = c + d;
+        double e = 1.1;
+        double f = 2.2;
+        double g = 3.3;
+        double h = 4.4;
+        double ef = e * f;
+        double gh = g * h;
+        int reszta = ab % b;
+        System.out.println("Reszta z dzielenia ab przez b to: " + reszta);
+        char i = 'A';
+        char j = 'a';
+        int ij = i + j;
+        System.out.println("Wartość dodawania zmiennych 'i' i 'j' wynosi: " + ij);
+        String k = "Ala";
+        System.out.println("Nie da się dodać zmiennych k i 1, ponieważ '1' nie może być nazwą zmiennej.");
+        boolean m = true;
+        boolean n = false;
+        System.out.println(m && n);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(ab);
+        System.out.println(cd);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(ef);
+        System.out.println(gh);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(m);
+        System.out.println(n);
+    }
+}
