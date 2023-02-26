1b7131da421066e9db953559d15c989141bdd184
Bartman18 <125701964+Bartman18@users.noreply.github.com>
2023-02-26 15:20:41 +0100

    Update Zadanie2.java

--- w1/zadania/Bartman18/Zadanie2.java
+++ w1/zadania/Bartman18/Zadanie2.java
@@ -1,5 +1,4 @@
 import java.math.RoundingMode;
-
 import static java.lang.Math.*;
 public class Zadanie2 {
     public static void main(String[] args) {
@@ -11,12 +10,20 @@ public class Zadanie2 {
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
-
+        
+        int e =-5;
         double g,h;
-        g=abs(E);
+        g=abs(e);
         h=10.501;
 
-        System.out.println(round(h));
+        System.out.println("Zaokrąglona wartość h = " + round(h));
 
+        System.out.println("Zmienna a ma wartosc: "+ a);
+        System.out.println("Zmienna b ma wartosc: "+ b);
+        System.out.println("Zmienna c ma wartosc: "+ c);
+        System.out.println("Zmienna d ma wartosc: "+ d);
+        System.out.println("Zmienna g ma wartosc: "+ g);
+        System.out.println("Zmienna h ma wartosc: "+ h);
     }
 }
+
