8bbe62a50a00bc989e43f7f9be55a15b84d69b3b
Bart3kTK <bkamieniarz55@wp.pl>
2023-02-26 17:57:44 +0100

    Rozwiazania zadan z zajec 1

---
+++ w1/zadania/Zadanie2.java
@@ -0,0 +1,30 @@
+public class Zadanie2 {
+    public static void main(String[] args){
+
+        int a = 5;
+        int b = 10;
+        double c = Math.pow(a,b);
+        double d = Math.pow(c,0.5);
+
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
+        System.out.println("c= "+c);
+        System.out.println("d= "+d);
+
+
+        int e = -5;
+        double g = Math.abs(e);
+        double h = 10.5001;
+        h = Math.round(h);
+
+        System.out.println("e= "+e);
+        System.out.println("g= "+g);
+        System.out.println("h= "+h);
+
+
+
+
+
+
+    }
+}
