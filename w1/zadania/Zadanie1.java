6550667634d7162f868c641455988823dca655a3
Manamo101 <122313886+Manamo101@users.noreply.github.com>
2023-02-26 15:30:18 +0100

    /Manamo101

---
+++ w1/zadania/Zadanie1.java
@@ -0,0 +1,35 @@
+public class Zadanie1
+{
+    public static void main(String[] args)
+    {
+        int a=1, b=2, c=3, d=4;
+        int ab=a+b, cd=c-d;
+        double e=1.1, f=2.2, g=3.3, h=4.4;
+        double ef=e*f, gh=f/h;
+        // ab mod b = 1
+        char i='A', j='a';
+        // i+j = 162
+        String k="Ala", l="ma";
+        // k+l = Alama
+        boolean m=true ,n=false;
+        System.out.println("m && n = " + (m && n));
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("ab = " + ab);
+        System.out.println("cd = " + cd);
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+        System.out.println("g = " + g);
+        System.out.println("h = " + h);
+        System.out.println("ef = " + ef);
+        System.out.println("gh = " + gh);
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+        System.out.println("l = " + l);
+        System.out.println("m = " + m);
+        System.out.println("n = " + n);
+    }
+}
