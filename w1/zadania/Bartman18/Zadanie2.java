77397f904eb1f83dfbfd7fcf18f9283eebac61a1
Bartosz Iwanicki <bartosz.iwanickii@gmail.com>
2023-02-26 15:09:20 +0100

    Rozwiązania zadań z zajęć 1

---
+++ w1/zadania/Bartman18/Zadanie2.java
@@ -0,0 +1,22 @@
+import java.math.RoundingMode;
+
+import static java.lang.Math.*;
+public class Zadanie2 {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        double c, d;
+        c=pow(a,b);
+        d=sqrt(c);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+        double g,h;
+        g=abs(E);
+        h=10.501;
+
+        System.out.println(round(h));
+
+    }
+}
