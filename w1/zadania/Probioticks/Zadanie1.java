ad7b771f78933c3ecd02574d51f4e3b51d8b478d
Dominik Nowicki <dominik.nowicki2004@gmail.com>
2023-02-27 16:32:12 +0100

    Rozwiązania zadań z zajęć 1

---
+++ w1/zadania/Probioticks/Zadanie1.java
@@ -0,0 +1,45 @@
+public class Zadanie1 {
+    public static void main(String[] args) {
+      int a = 1;
+      int b = 2;
+      int c = 3;
+      int d = 4;
+      int ab = a + b;
+      int cd = c + d;
+      double  e,f,g,h;
+      e = 1.1;
+      f = 2.2;
+      g = 3.3;
+      h = 4.4;
+      double  ef = e + f;
+        double gh = g + h;
+        float modulo = ab % b;
+        char i = 'A';
+        char j = 'a';
+         //nie można dodawac zmiennych char oraz string
+
+
+        boolean m = true, n = false;
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
+      System.out.println(d);
+      System.out.println(ab);
+      System.out.println(cd);
+      System.out.println(e);
+      System.out.println(f);
+      System.out.println(g);
+      System.out.println(h);
+      System.out.println(ef);
+      System.out.println(gh);
+      System.out.println(modulo);
+      System.out.println(i);
+      System.out.println(j);
+
+
+
+
+
+
+    }
+}
