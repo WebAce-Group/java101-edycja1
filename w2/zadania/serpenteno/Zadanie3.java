17df549cd72432b8dfa1b27787497104fcfc7306
serpenteno <125379613+serpenteno@users.noreply.github.com>
2023-02-27 22:35:56 +0100

    Update Zadanie3.java

--- w2/zadania/serpenteno/Zadanie3.java
+++ w2/zadania/serpenteno/Zadanie3.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Zadanie3 {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj boki trojkata: ");
+        System.out.println("Podaj boki trójkąta: ");
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int c = scanner.nextInt();
         
         if(a >= b + c || b >= a + c || c >= a + b) {
-            System.out.println("Nie mozna utworzyc trojkata :(");
+            System.out.println("Nie można utworzyć trójkąta :(");
         } else if (a==b && a==c) {
-            System.out.println("Trojkat rownoboczny");
+            System.out.println("Trójkąt równoboczny");
         } else if (a!=b && a!=c && b!=c) {
-            System.out.println("Trojkat roznoboczny");
+            System.out.println("Trójkąt różnoboczny");
         } else {
-            System.out.println("Trojkat rownoramienny");
+            System.out.println("Trójkąt równoramienny");
         }
     }
 }
