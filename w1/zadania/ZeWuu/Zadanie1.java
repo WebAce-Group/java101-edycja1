f67b236e4081c30825244fb7e5100d72be8b471e
ZeWuu <125304100+ZeWuu@users.noreply.github.com>
2023-02-28 13:40:39 +0100

    Rozwiązania zadań z zajęć 1 (#49)

---
+++ w1/zadania/ZeWuu/Zadanie1.java
@@ -0,0 +1,63 @@
+public class Zadanie1 {
+    public static void main(String[] args) {
+         int a,b,c,d;
+         a=1;
+         b=2;
+         c=3;
+         d=4;
+         
+         int ab,cd;
+         ab=a+b;
+         cd=c-d;
+
+         double e,f,g,h;
+         e=1.1;
+         f=2.2;
+         g=3.3;
+         h=4.4;
+
+         double ef,gh;
+         ef=e*f;
+         gh=g/h;
+
+         System.out.println("Reszta z dzielenie ab przez b: " + (ab%b));
+
+         char i,j;
+         i='A';
+         j='a';
+
+         System.out.println("Suma i+j=" +(i+j)); //Dodajemy kody ASCII
+
+         String k,l;
+         k="Ala";
+         l="ma";
+
+         System.out.println("Suma k+l=" +(k+l)); //Dodajemy dwa stringi tworząc z nich jeden string
+
+         boolean m,n;
+         m=true;
+         n=false;
+
+         System.out.println("m && n=" +(m&&n));
+
+         System.out.println("a=" +a);
+         System.out.println("b=" +b);
+         System.out.println("c=" +c);
+         System.out.println("d=" +d);
+         System.out.println("ab=" +ab);
+         System.out.println("cd=" +cd);
+         System.out.println("e=" +e);
+         System.out.println("f=" +f);
+         System.out.println("g=" +g);
+         System.out.println("h=" +h);
+         System.out.println("ef=" +ef);
+         System.out.println("gh=" +gh);
+         System.out.println("i=" +i);
+         System.out.println("j=" +j);
+         System.out.println("k=" +k);
+         System.out.println("l=" +l);
+         System.out.println("m=" +m);
+         System.out.println("n=" +n);
+    }
+}
+
