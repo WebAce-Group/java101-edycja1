a1025c6c0ed2b829439294b6fdfa52966c9af7b5
Gukson <75785907+Gukson@users.noreply.github.com>
2023-02-28 07:02:02 +0100

    test

--- w2/zadania/Gukson/zadanie8.java
+++
@@ -1,9 +0,0 @@
-public class zadanie8 {
-    public static void main(String[] args) {
-        for (int x = 1;x<=100;x++){
-            if(x == 5)break;
-            if(x%3 != 0)continue;
-            System.out.println("x = " + x);
-        }
-    }
-}
