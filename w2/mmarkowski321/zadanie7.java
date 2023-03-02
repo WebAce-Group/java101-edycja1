1be880d77bf3be32cc6623e17c6d83300af086a2
mmarkowski321 <markowskimilosz989@gmail.com>
2023-03-02 18:34:13 +0100

    Rozwiązania zadań z zajęć 2

---
+++ w2/mmarkowski321/zadanie7.java
@@ -0,0 +1,18 @@
+package w2.mmarkowski321;
+import java.util.Scanner;
+public class zadanie7 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int liczbaProb=0;
+        int suma=0;
+        int liczba;
+        do {
+            System.out.println("Podaj liczbe");
+            liczba = scanner.nextInt();
+            suma+=liczba;
+            liczbaProb++;
+        }while (liczba <= 100);
+        System.out.println();
+        System.out.println(suma/liczbaProb);
+    }
+}
