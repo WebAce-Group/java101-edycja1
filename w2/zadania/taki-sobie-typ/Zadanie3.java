78988b44adee353a37f81f44c2ae4ad7dded5ceb
taki-sobie-typ <ogorek610@gmail.com>
2023-03-06 20:20:29 +0100

    Rozwiazania zadan wyklad 2

---
+++ w2/zadania/taki-sobie-typ/Zadanie3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Zadanie3 {
+    public static void main(String[] args){
+        int a,b,c;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj bok a: ");
+        a = scanner.nextInt();
+        System.out.println("Podaj bok b: ");
+        b = scanner.nextInt();
+        System.out.println("Podaj bok c: ");
+        c = scanner.nextInt();
+
+        if(a+b>c || a+c>b || c+b>a){
+            System.out.println("Istnieje!\n");
+            if(a==b && c==a){
+                System.out.println("Jest rownoboczny");
+            }
+            else if(a==b || c==a || b==c){
+                System.out.println("Jest rownoramienny");
+            }
+            else {
+                System.out.println("Jest roznoboczny");
+            }
+        }
+        else{
+            System.out.println("Nie da sie zbudowac!");
+        }
+
+    }
+}
---
+++ w2/zadania/taki-sobie-typ/Zadanie7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Zadanie7 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        boolean warunek = true;
+        int liczba, licznik=0, suma = 0;
+        System.out.println("Wpisz calkowite, (liczba>100)=stop");
+
+        do{
+            licznik++;
+            if((liczba = scanner.nextInt())<=100){
+                System.out.println((suma+=liczba)/licznik);
+            }
+            else {
+                warunek = false;
+            }
+        }while (warunek);
+    }
+}
