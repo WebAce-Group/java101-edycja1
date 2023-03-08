3e07ba17ff1bec5292f75391201a8861cea91b1e
Piotrwroc <126483723+Piotrwroc@users.noreply.github.com>
2023-03-09 00:19:29 +0100

    Add files via upload

---
+++ w2/zadania/Piotrwroc/Zadanie7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Zadanie7 {
+    public static void main(String[] args)
+    {
+        Scanner liczba = new Scanner(System.in);
+        int suma=0;
+        int ilosc=0;
+        System.out.println("Petla dziala do podania liczby wiekszej od 100");
+        do
+        {
+            int x = liczba.nextInt();
+            if (x>100) break;
+            suma+=x;
+            ilosc++;
+        }while(true);
+        System.out.println(suma/ilosc);
+    }
+}
