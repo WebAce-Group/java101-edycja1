c57152483e67348ec60ae2672314563c4ab8790f
Maciej Siwicki <maciej.siwicki00@gmail.com>
2023-03-09 22:26:49 +0100

    Revert "Rozwiązanie zadań z zajęć 2"
    This reverts commit f4b9ffe47ffa3e39d754d48c24533d7d3cdc5a5d.

--- w2/zadania/msiwicky/Zadanie8.java
+++ w2/zadania/msiwicky/Zadanie8.java
@@ -1,11 +1,2 @@
-public class Zadanie8 {
-    public static void main(String[] args) {
-        for (int i = 1; i < 101; i++) {
-            if (i == 50)
-                break;
-            if (i % 3 != 0)
-                continue;
-            System.out.println(i);
-        }
-    }
+package PACKAGE_NAME;public class Zadanie8 {
 }
