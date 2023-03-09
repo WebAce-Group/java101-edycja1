caff89c39aaab4924a09e1b97661a0f9bb988aff
Nurek54 <g_sienkiewicz@o2.pl>
2023-03-09 20:40:50 +0100

    Poprawa Zadania 7

--- w2/zadania/Nurek54/Zadanie7.java
+++ w2/zadania/Nurek54/Zadanie7.java
@@ -1,42 +1,26 @@
 package w2.zadania.Nurek54;
 import java.util.Scanner;
+
 public class Zadanie7 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
         int sum = 0;
         int count = 0;
+        int number;
 
-        while (true) {
-            System.out.print("Podaj liczbę całkowitą mniejszą od 100: ");
-            int number = input.nextInt();
+        do {
+            System.out.print("Podaj liczbę całkowitą: ");
+            number = scanner.nextInt();
 
-            if (number < 100) {
+            if (number <= 100) {
                 sum += number;
                 count++;
-                break;
-            } else {
-                System.out.println("Podana liczba jest >= 100. Spróbuj ponownie.");
-            }
-        }
-
-        while (true) {
-            System.out.print("Podaj kolejną liczbę całkowitą: ");
-            int number = input.nextInt();
-
-            if (number < 100) {
-                sum += number;
-                count++;
-            } else {
-                System.out.println("Podana liczba jest >= 100. Spróbuj ponownie.");
             }
 
-            if (count > 0 && number >= 100) {
-                break;
-            }
-        }
+        } while (number <= 100);
 
         double average = (double) sum / count;
-        System.out.println("Średnia arytmetyczna podanych liczb to: " + average);
+        System.out.println("Średnia arytmetyczna podanych liczb: " + average);
     }
 }
+
