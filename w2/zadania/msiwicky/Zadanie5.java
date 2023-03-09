bc594cfb8930bd0049759ad11e11a5340eafc6f8
Maciej Siwicki <maciej.siwicki00@gmail.com>
2023-03-09 22:45:36 +0100

    Add new folder

---
+++ w2/zadania/msiwicky/Zadanie5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Zadanie5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj liczbe n: ");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++)
+            System.out.print(i + " ");
+        System.out.println();
+        for (int i = n; i > 0; i--)
+            System.out.print(i + " ");
+        System.out.println();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0)
+                System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0){
+                if (i % 3 != 0 || i % 5 != 0)
+                    System.out.print(i + " ");
+            }
+        }
+
+    }
