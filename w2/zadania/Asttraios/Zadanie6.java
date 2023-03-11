870e7daead53bf94ca21cfb66e52101fc6602b7f
Krzysztof Swiderski <krzysiek2015swiderski@gmail.com>
2023-03-11 23:57:06 +0100

    Poprawione zadania 6 i 7

--- w2/zadania/Asttraios/Zadanie6.java
+++ w2/zadania/Asttraios/Zadanie6.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Zadanie6 {
     public static void main(String [] args) {
         
-        int a;
+        int a, suma=0;
         boolean prawda=true;
         Scanner input4=new Scanner(System.in);
 
@@ -13,11 +13,13 @@ public class Zadanie6 {
 
             System.out.println("Podaj liczbe calkowita: ");
             a=input4.nextInt();
-            
+            suma=+a;
             if(a==0)    prawda=false;
 
         }
         input4.close();
+
+        System.out.println("Suma= "+suma);
     }
     
 }
--- w2/zadania/Asttraios/Zadanie7.java
+++ w2/zadania/Asttraios/Zadanie7.java
@@ -5,15 +5,18 @@ public class Zadanie7 {
     public static void main(String [] args) {
          
         Scanner input5= new Scanner(System.in);
-        int b;
+        int b, licznik=0, suma=0;;
 
         do{
                 System.out.println("Podaj liczbe calkowita: ");
                  b=input5.nextInt();
-                
+                suma=+b;
+                 licznik++;
 
         }while(b<=100);
         input5.close();
+
+        System.out.println("Srednia= "+ (suma/licznik));
 
     }
 }
