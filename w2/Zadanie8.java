738311dd2168b9f15a0e21b273464638425348fe
TheMethier <106596805+TheMethier@users.noreply.github.com>
2023-03-12 18:25:25 +0100

    Rozwiązanie zadań z wykładu 2

---
+++ w2/Zadanie8.java
@@ -0,0 +1,11 @@
+public class Zadanie8
+{
+    public static void main(String arg[])
+    {
+        for (int i=1;i<=100;i++)
+        {
+            if(i%3==0) System.out.println(i);
+            if(i==50)break;
+        }
+    }
+}
