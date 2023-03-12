c300662898222072e58b77c481673f22093b77b2
Maciej Binczarowski <maciej.binczarowski@gmail.com>
2023-03-12 11:49:10 +0100

    Dodanie pliku Zadanie4 i wykonanie pierwszej części

---
+++ w2/zadania/MaciejBinczarowski/Zadanie4.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+class Zadanie4{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        switch(scanner.nextInt()){
+            case 1:
+                System.out.println("Zima chyba");
+                break;
+            
+            case 2:
+                System.out.println("Zima chyba");
+                break;
+
+            case 3:
+                System.out.println("Zima chyba");
+                break;
+
+            case 4:
+                System.out.println("Wiosna");
+                break;
+
+            case 5:
+                System.out.println("Wiosna");
+                break;
+
+            case 6:
+                System.out.println("Wiosna");
+                break;
+
+            case 7:
+                System.out.println("Lato");
+                break;
+
+            case 8:
+                System.out.println("Lato");
+                break;
+
+            case 9:
+                System.out.println("Lato");
+                break;
+
+            case 10:
+                System.out.println("Jesień");
+                break;
+
+            case 11:
+                System.out.println("Jesień");
+                break;
+
+            case 12:
+                System.out.println("Jesień");
+                break;
+
+            default:
+                System.out.println("Nie ma takiego miesiąca, no co Ty");
+        }
+    }
+}
