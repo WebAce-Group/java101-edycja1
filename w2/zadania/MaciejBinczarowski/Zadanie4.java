ad5faf77ecab6d09668b62b149cd5e4c9093d1ce
Maciej Binczarowski <maciej.binczarowski@gmail.com>
2023-03-12 13:01:41 +0100

    Dokończenie zadania4

--- w2/zadania/MaciejBinczarowski/Zadanie4.java
+++ w2/zadania/MaciejBinczarowski/Zadanie4.java
@@ -56,11 +56,37 @@ class Zadanie4{
                 System.out.println("Nie ma takiego miesiąca, no co Ty");
         }
 
+        // Nie wiem czy poniższy fragment działa, ponieważ mam za starą wersję javy i nie potrafię jej zaktualizować :(
+        // ale chyba jest git xd
+
         // String month;
-        // String month = switch(scanner.nextInt()){
+        // month = switch(scanner.nextInt()){
         //     case 1:
         //         yield "Poniedziałek";
+
+        //     case 2:
+        //         yield "Wtorek";
+
+        //     case 3:
+        //         yield "Środa";
+
+        //     case 4:
+        //         yield "Czwartek";
+
+        //     case 5:
+        //         yield "Piątek";
+
+        //     case 6:
+        //         yield "Sobota";
+
+        //     case 7:
+        //         yield "Niedziela";
+
+        //     default:
+        //         yield "Nie właściwa wartość"
         // }
+
+        // System.out.println(month);
 
         scanner.close();
     }
