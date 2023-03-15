83f0f0ed8d3d7cc355ebe3a3bcaa5917472ea5d5
Antoni Golachowski <91195241+AntekGol@users.noreply.github.com>
2023-03-15 12:16:33 +0100

    Update Zadanie7.java

--- w2/zadania/AntekGol/Zadanie7.java
+++ w2/zadania/AntekGol/Zadanie7.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Zadanie7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int suma=0;
-        int liczb=0;
+        double suma=0;
+        double liczb=0;
         int n;
         do {
             System.out.print("Podaj liczbe wieksza od 100"+"\n");
