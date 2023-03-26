1d8507f72e3deca08d4685920590764d781c20cb
Maciej Binczarowski <maciej.binczarowski@gmail.com>
2023-03-26 21:15:34 +0200

    Dodanie zadania dodatkowego

--- w3/zadania/MaciejBinczarowski/ZadanieDodatkowe.java
+++ w3/zadania/MaciejBinczarowski/ZadanieDodatkowe.java
@@ -1,17 +1,54 @@
+import java.util.Arrays;
+
 public class ZadanieDodatkowe
 {
+    /*
+     * @brief The method spinWords make rversed all words in sentence, wich lenght is
+     *        is longer than 5 letters.
+     * 
+     * @param Method takes only one string argument, wich is sentence.
+     * 
+     * @return The "spinned" string sentence.
+     */
+    private static String spinWords(final String sentence)
+    {
+        final String[] words = sentence.split(" ");
+        String[] letters;
+        String spinnedWord;
+        String spinnedSentence = "";
+
+        for (String word : words) 
+        {
+            spinnedWord = "";
+
+            if (word.length() >= 5)
+            {
+                letters = word.split("");
+                
+                for (int i = letters.length - 1; i >= 0; i--)
+                {
+                    spinnedWord += letters[i];
+                }
+            }
+            else
+            {
+                spinnedWord = word;
+            }
+
+            spinnedSentence += spinnedWord + " ";
+        }
+        return spinnedSentence;
+    }
     public static void main(String[] args)
     {
-        final String a = "abba,b,c";
-        String[] abc = a.split(",");
-        String[] abcd;
-        for (String string : abc) {
-            abcd = string.split("");
-            for (String string2 : abcd) {
-                System.out.println(string2);
-            }
+        System.out.println(spinWords("Ala ma kota, kot ma Alę"));
+        System.out.println(spinWords("Hey fellow warriors"));
+        System.out.println(spinWords("This is a test"));
+        System.out.println(spinWords("This is another test"));
+
+        for (String string : args) 
+        {
+            System.out.println(spinWords(string));
         }
-
-
     }
 }
