685e6dff57c2efb89b2507739bd57885c644a40d
Swagrzyk <mswagrzyk@interia.pl>
2023-03-29 16:21:00 +0200

    Rozwiazanie zadan 4

---
+++ w4/zadania/Swagrzyk/zadanie14.java
@@ -0,0 +1,8 @@
+public class zadanie14 {
+    public static void main(String[] args) {
+        Rectangle prostokat =new Rectangle(14,10);
+        System.out.println(prostokat.getArea());
+        System.out.println(prostokat.getPerimeter());
+
+    }
+}
