0d9964be9f49605ecce1abc4e7e028bdb242159e
Maciej Binczarowski <maciej.binczarowski@gmail.com>
2023-04-02 17:23:48 +0200

    Zadania 14, 15, 16

---
+++ w4/zadania/MaciejBinczarowski/Zadanie15/Animal.java
@@ -0,0 +1,4 @@
+public abstract class Animal
+{
+    public abstract void makeSound();
+}
---
+++ w4/zadania/MaciejBinczarowski/Zadanie15/Dog.java
@@ -0,0 +1,7 @@
+public class Dog extends Animal
+{
+    public void makeSound()
+    {
+        System.out.println("Woof");
+    }
+}
