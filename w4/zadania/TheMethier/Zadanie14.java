d65655e5d5d3f9bb3c8fba10537f44e62a5d6a3c
TheMethier <106596805+TheMethier@users.noreply.github.com>
2023-04-03 01:28:01 +0200

    Zadania z zajęć 4

---
+++ w4/zadania/TheMethier/Zadanie14.java
@@ -0,0 +1,26 @@
+
+     class Rectangle
+    {
+        float length;
+        float width;
+        public Rectangle(float length,float width)
+        {
+            length=this.length;
+            width=this.width;
+        }
+        public float getArea()
+        {
+            return length*width;
+        }
+        public float getPerimiter()
+        {
+            return 2*length+2*width;
+        }
+
+    }
+    public class Zadanie14{
+    public static void main(String[] args) {
+
+    }
+
+}
