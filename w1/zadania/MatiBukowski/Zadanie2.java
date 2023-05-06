3f4c21cfa4a66cf85131e71fbcc92401c1cc626f
MatiBukowski <mati.bukowski@icloud.com>
2023-05-06 22:39:56 +0200

    Zadania z wykładu 1

---
+++ w1/zadania/MatiBukowski/Zadanie2.java
@@ -0,0 +1,26 @@
+package w1.zadania.MatiBukowski;
+
+import static java.lang.Math.*;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        double c = pow(a, b);
+        double d = sqrt(c);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        int e = -5;
+        double g = abs(e);
+        double h = 10.5001;
+        h = round(h);
+        System.out.println(e);
+        System.out.println(g);
+        System.out.println(h);
+
+    }
+
+
+}
